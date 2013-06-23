439a3c2d16fb1af70bd47eb51df8183f78531e15
martincz <martincz@mokeetek.com>
2013-06-24 04:29:46 +0800

    MoKeeLauncher:merge CyanogenMod
    Change-Id: I33b796421ab28003edc74010a230758baa34294a

--- src/com/mokee/launcher/LauncherModel.java
+++ src/com/mokee/launcher/LauncherModel.java
@@ -46,7 +46,7 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.util.Log;
 
-import com.cyanogenmod.trebuchet.preference.PreferencesProvider;
+import com.mokee.launcher.preference.PreferencesProvider;
 
 import java.lang.ref.WeakReference;
 import java.net.URISyntaxException;
